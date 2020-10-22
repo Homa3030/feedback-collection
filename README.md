15a35cf4b5ffddea97158ac95e09b728678b88c7
Regina Khamatova <homa30302000@gmail.com>
2020-10-22 19:16:31 +0300

    Add models, create endpoint to save form as a template

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # feedback-collection
 
 This is a Feedback Collection project for FSE course.
+
+## After the database models are updated, run:
+```
+python -m flask db migrate
+python -m flask db upgrade
+```
+
+This will create a migration with up-to-date data models and modify the database correspondingly.
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, url_for, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user, login_user, logout_user, UserMixin
+from flask_migrate import Migrate
+import os
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 app = Flask(__name__, instance_relative_config=False)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('FEEDBACK_DB', 'error')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
 
 # Initialize Plugins
 db.init_app(app)
@@ -32,6 +35,42 @@ class User(UserMixin, db.Model):
     # 2 - Student
     def __repr__(self):
         return '%s %s, %s' % (self.name, self.surname, self.mail)
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String, nullable=False)
+    answers = db.Column(db.ARRAY(db.String), nullable=False)
+    template_id = db.Column(db.Integer, db.ForeignKey('form_template.id'), nullable=False)
+    #If question is open-ended then answers = {} (empty array)
+
+class FormTemplate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    questions = db.relationship('Question', backref='form_template')
+    visible = db.Column(db.Boolean, nullable=False)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    template_id = db.Column(db.Integer, db.ForeignKey('form_template.id'))
+
+@app.route('/save_form_as_template/<form_id>', methods=['POST'])
+def save_form_as_template(form_id):
+    form_template_id = Form.query.get(form_id).template_id
+
+    new_form_template = FormTemplate()
+    new_form_template.visible = True
+    db.session.add(new_form_template)
+
+    questions = FormTemplate.query.get(form_template_id).questions
+    
+    for question in questions:
+        new_question = Question()
+        new_question.question = question.question
+        new_question.answers = question.answers
+        new_question.template_id = new_form_template.id
+        db.session.add(new_question)
+
+    db.session.commit()
+
 
 @login_manager.user_loader
 def load_user(user_id):
